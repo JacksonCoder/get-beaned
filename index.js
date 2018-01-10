bdd59dbda26713a8fe5741425e2d6f607a1ce5a9
Jackson Lewis <st.japa6@gmail.com>
2018-01-10 15:42:16 -0800

    Added script

---
+++ index.js
@@ -0,0 +1,8 @@
+window.setInterval(function()
+	{
+$("img").attr('src','https://pbs.twimg.com/profile_images/774215859355123712/sqnz1MX0_400x400.jpg');
+$("img").attr('srcset','https://pbs.twimg.com/profile_images/774215859355123712/sqnz1MX0_400x400.jpg');
+$("img").attr('data-src','https://pbs.twimg.com/profile_images/774215859355123712/sqnz1MX0_400x400.jpg');
+	},
+	1000
+)
