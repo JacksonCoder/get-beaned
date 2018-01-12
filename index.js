14c55d61e9262a0a4f32df3ee588d2d40de0ad8d
circuitbird <33707046+circuitbird@users.noreply.github.com>
2018-01-11 16:42:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.setInterval(function()
 {
 $("img").attr('src','http://i0.kym-cdn.com/entries/icons/original/000/021/229/ss_(2016-09-04_at_03.45.12).jpg'); //http://www.thicc.tech/img/beaned.jpg
-//$("img").attr('data-src','http://i0.kym-cdn.com/entries/icons/original/000/021/229/ss_(2016-09-04_at_03.45.12).jpg'); (not necessary)
 },
 	1000
 )
